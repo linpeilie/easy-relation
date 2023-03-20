4b0bf0d1a163fedfd745fb4758506f02be5d5e65
linpeilie <linpeilie@qq.com>
2023-03-20 19:29:28 +0800

    添加百度统计

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import {defineUserConfig} from "vuepress";
 import {shikiPlugin} from "@vuepress/plugin-shiki";
 import {searchPlugin} from "@vuepress/plugin-search";
 // import {googleAnalyticsPlugin} from "@vuepress/plugin-google-analytics"
-// import { searchConsolePlugin } from 'vuepress-plugin-china-search-console'
+import { searchConsolePlugin } from 'vuepress-plugin-china-search-console'
 
 import theme from "./theme.js";
 
@@ -19,7 +19,7 @@ export default defineUserConfig({
 //               s.parentNode.insertBefore(hm, s);
 //             })();
 //         `],
-//         ['meta', {name: 'baidu-site-verification', content: 'codeva-OceTRzMGJ2'}],
+        ['meta', {name: 'baidu-site-verification', content: 'codeva-DlTJUnIVzh'}],
 //         ['meta', {name: 'bytedance-verification-code', content: 'fXSJ80ILSanHaf/RIZGQ'}],
     ],
 
@@ -43,9 +43,8 @@ export default defineUserConfig({
 //         googleAnalyticsPlugin({
 //             id: 'G-SXEZVNR8FZ'
 //         }),
-//         searchConsolePlugin({
-//             baiduId: 'fab881821b3db8a7c460db1c91ea0f3a',
-//             toutiaoAutoPushId: '866dcc2ae0f9a8ffdfd3f1b664eafc71e58e4febb19fd7d283afc51c8e2dd61dfd9a9dcb5ced4d7780eb6f3bbd089073c2a6d54440560d63862bbf4ec01bba3a'
-//         })
+        searchConsolePlugin({
+            baiduId: 'eb35e18caa552284b39d427c1e06f9f7'
+        })
     ]
 });
